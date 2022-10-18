7ec375162f32c625b0b18d2a8c709e7c55132ce9
Stuart Frost <stuart.2.frost@bt.com>
2022-10-18 20:17:50 +0100

    print currency exchanges in cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,34 @@
 <script>
+    import { current_component } from "svelte/internal";
+
   let data;
 
   const fetchData = async () => {
     data = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
                  .then((response) => response.json())
-                 .then((json) => JSON.stringify(json, null, 1))
   }
 </script>
 
 <main>
-  <div>
   <h1>DevCon - Svelte</h1>
 
   <div class="card">
     <button on:click={fetchData}>Fetch Bitcoin exchange rates</button>
 
-    <pre>
-      {data}
-    </pre>
+    {#if data}
+      <h2>{data.chartName} Exchange Rates</h2>
+      <i>Last updated: {data.time.updated}</i>
+
+      <div class="d-flex">
+        {#each Object.values(data.bpi) as currency}
+          <div class="card">
+            <h3>{currency.code}</h3>
+            <i>{currency.description}</i>
+
+            <p>{currency.rate}</p>
+          </div>
+        {/each}
+      </div>
+    {/if}
   </div>
 </main>
