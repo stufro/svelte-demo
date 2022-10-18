29699107d12e5fa9468a63bc2e076dd69ad14131
Stuart Frost <stuart.2.frost@bt.com>
2022-10-18 19:51:28 +0100

    fetch data and splat JSON on screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
+<script>
+  let data;
+
+  const fetchData = async () => {
+    data = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+                 .then((response) => response.json())
+                 .then((json) => JSON.stringify(json, null, 1))
+  }
+</script>
+
 <main>
   <div>
   <h1>DevCon - Svelte</h1>
 
   <div class="card">
+    <button on:click={fetchData}>Fetch Bitcoin exchange rates</button>
 
+    <pre>
+      {data}
+    </pre>
   </div>
 </main>
